bccd07a5b78563155a7517d0243edc429e7b34cd
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-10-31 19:51:45 +0100

    agregando barra de vidas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
-    private int vidasJugador = 3;
+    private int VIDAS_JUGADOR = 5;
+    private int vidaJugador;
+    public Image barraDeVidas;
+
+    public Sprite VidaMediaLlena;
+    public Sprite VidaMitad;
+    public Sprite VidaMediaVacia;
+    public Sprite VidaVacia;
 
     public static GameManager instance;
 
@@ -23,15 +32,38 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public int getVidasJugador { get {return vidasJugador;}}
+    void Start(){
+         vidaJugador = VIDAS_JUGADOR;
+    }
+
+    public int getVidasJugador { get {return VIDAS_JUGADOR;}}
 
 
     public void quitarVidaJugador(){
-        vidasJugador--;
+        vidaJugador--;
+    }
+
+    void actualizarBarraVida(){
+        switch (vidaJugador) {
+            case 4: 
+            barraDeVidas.sprite = VidaMediaLlena;
+            break;
+            case 3: 
+            barraDeVidas.sprite = VidaMitad;
+            break;
+            case 2:
+            barraDeVidas.sprite = VidaMediaVacia;
+            break;
+            case 1:
+            barraDeVidas.sprite = VidaVacia;
+            break;
+        }
     }
 
     public void comprobarVidaJugador(){
-        if(vidasJugador <= 0){
+        Debug.Log("hola");
+        actualizarBarraVida();
+        if(vidaJugador <= 0){
             Debug.Log("Game Over");
         }
     }
